c458df7543da03fd0bfc6787a3d2eefe1c862b77
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-05-31 23:02:21 +0100

    cc

--- checkout.js
+++ checkout.js
@@ -1,21 +1,21 @@
 function openPaymentModal(publicKey, secretKey, amount, currency) {
-        // Create the payment modal iframe
-        var iframe = document.createElement("iframe");
-        iframe.src = `https://Widget-paylode.netlify.app?publicKey=${encodeURIComponent(
-          publicKey
-        )}&secretKey=${encodeURIComponent(
-          secretKey
-        )}&amount=${encodeURIComponent(amount)}&currency=${encodeURIComponent(
-          currency
-        )}`;
-        iframe.style.border = "none";
-        iframe.style.width = "100%";
-        iframe.style.height = "100vh";
-        iframe.style.position = "fixed";
-        iframe.style.top = "0";
-        iframe.style.left = "0";
-        iframe.style.zIndex = "9999";
+  // Create the payment modal iframe
+  var iframe = document.createElement("iframe");
+  iframe.src = `https://Widget-paylode.netlify.app?publicKey=${encodeURIComponent(
+    publicKey
+  )}&secretKey=${encodeURIComponent(secretKey)}&amount=${encodeURIComponent(
+    amount
+  )}&currency=${encodeURIComponent(currency)}`;
+  iframe.style.border = "none";
+  iframe.style.width = "100%";
+  iframe.style.height = "100vh";
+  iframe.style.position = "fixed";
+  iframe.style.top = "0";
+  iframe.style.left = "0";
+  iframe.style.zIndex = "9999";
 
-        // Append the iframe to the body
-        document.body.appendChild(iframe);
-      }
+  // Append the iframe to the body
+  document.body.appendChild(iframe);
+}
+
+export default openPaymentModal;
