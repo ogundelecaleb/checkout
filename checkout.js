586723bddc9a72f1a65dd26bc497b51b642cf8d8
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-06-02 21:48:17 +0100

    uu

--- checkout.js
+++ checkout.js
@@ -1,20 +1,32 @@
-function openPaymentModal(publicKey, secretKey, amount, currency) {
-  // Create the payment modal iframe
-  var iframe = document.createElement("iframe");
-  iframe.src = `https://Widget-paylode.netlify.app?publicKey=${encodeURIComponent(
-    publicKey
-  )}&secretKey=${encodeURIComponent(secretKey)}&amount=${encodeURIComponent(
-    amount
-  )}&currency=${encodeURIComponent(currency)}`;
-  iframe.style.border = "none";
-  iframe.style.width = "100%";
-  iframe.style.height = "100vh";
-  iframe.style.position = "fixed";
-  iframe.style.top = "0";
-  iframe.style.left = "0";
-  iframe.style.zIndex = "9999";
+const PaylodeCheckout = {
+  records: undefined,
+  setup: function (data) {
+    // Create the payment modal iframe
+    records = data;
+    if(data.onClose != undefined){
+        data.onClose();
+    }
+    //data.onClose();
+    //data.callback();
+    console.log(records);
+    return this;
+  },
+  openIframe: function () {
+    var iframe = document.createElement("iframe");
+    iframe.src = `https://Widget-paylode.netlify.app?publicKey=${encodeURIComponent(
+      records.publicKey
+    )}&amount=${encodeURIComponent(
+      records.amount
+    )}&currency=${encodeURIComponent(records.currency)}`;
+    iframe.style.border = "none";
+    iframe.style.width = "100%";
+    iframe.style.height = "100vh";
+    iframe.style.position = "fixed";
+    iframe.style.top = "0";
+    iframe.style.left = "0";
+    iframe.style.zIndex = "9999";
 
-  // Append the iframe to the body
-  document.body.appendChild(iframe);
-}
-
+    // Append the iframe to the body
+    document.body.appendChild(iframe);
+  },
+};
