917e7c2061c5f90ff61db11c54e9eba33a5fabdf
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-06-03 17:31:55 +0100

    new

--- checkout.js
+++ checkout.js
@@ -1,32 +1,56 @@
 const PaylodeCheckout = {
-  records: undefined,
-  setup: function (data) {
-    // Create the payment modal iframe
-    records = data;
-    if(data.onClose != undefined){
-        data.onClose();
+    records: undefined,
+    onCloseCallback: undefined,
+    setup: function (data) {
+      // Create the payment modal iframe
+      records = data;
+      // if (data.onClose != undefined) {
+      //   data.onClose();
+      // }
+      //data.onClose();
+      //data.callback();
+      this.onCloseCallback = data.onClose;
+      console.log(records);
+      return this;
+    },
+    openIframe: function () {
+      var onCloseCallbackStr = this.onCloseCallback ? this.onCloseCallback.toString() : '';
+  
+  
+      var iframe = document.createElement("iframe");
+      iframe.src = `https://Widget-paylode.netlify.app?publicKey=${encodeURIComponent(
+        records.publicKey
+      )}&amount=${encodeURIComponent(
+        records.amount
+      )}&currency=${encodeURIComponent(records.currency)}&onCloseCallback=${encodeURIComponent(onCloseCallbackStr)}`;
+      iframe.style.border = "none";
+      iframe.style.width = "100%";
+      iframe.style.height = "100vh";
+      iframe.style.position = "fixed";
+      iframe.style.top = "0";
+      iframe.style.left = "0";
+      iframe.style.zIndex = "9999";
+  
+      // Append the iframe to the body
+      document.body.appendChild(iframe);
+  
+      // Listen for messages from the iframe
+      window.addEventListener("message", this.receiveMessage.bind(this), false);
+      
+    },
+    receiveMessage: function (event) {
+      // Check if the message is from the iframe and contains the expected data
+      if (
+        // event.data.source === "Widget-paylode" &&
+        // event.data.type === "closeModal" &&
+        typeof this.onCloseCallback === "function"
+      ) {
+        // Call the onClose callback function with the desired return values
+        this.onCloseCallback(event.data.data);
+  
+        // Remove the iframe from the body
+        document.body.removeChild(event.source.frameElement);
+      }
     }
-    //data.onClose();
-    //data.callback();
-    console.log(records);
-    return this;
-  },
-  openIframe: function () {
-    var iframe = document.createElement("iframe");
-    iframe.src = `https://Widget-paylode.netlify.app?publicKey=${encodeURIComponent(
-      records.publicKey
-    )}&amount=${encodeURIComponent(
-      records.amount
-    )}&currency=${encodeURIComponent(records.currency)}`;
-    iframe.style.border = "none";
-    iframe.style.width = "100%";
-    iframe.style.height = "100vh";
-    iframe.style.position = "fixed";
-    iframe.style.top = "0";
-    iframe.style.left = "0";
-    iframe.style.zIndex = "9999";
-
-    // Append the iframe to the body
-    document.body.appendChild(iframe);
-  },
-};
+  };
+  
