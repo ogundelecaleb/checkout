fe5fb53bb13cbb232ea33bea2092039ee8b26332
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-06-13 00:05:39 +0100

    encryp by raphael

--- checkout.js
+++ checkout.js
@@ -1,16 +1,12 @@
-// const crypto = require('crypto');
-
-// import { encrypt } from 'n-krypta';
-
+import * as CryptoJS from "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js";
 const PaylodeCheckout = {
- 
   records: undefined,
   onCloseCallback: undefined,
   onSuccessCallback: undefined,
   setup: function (data) {
     // Create the payment modal iframe
     records = data;
-   
+
     this.onSuccessCallback = data.onSuccess;
     this.onCloseCallback = data.onClose;
     console.log(records);
@@ -25,11 +21,38 @@ const PaylodeCheckout = {
       ? this.onSuccessCallback.toString()
       : "";
 
-var key = "1"
-    this.encrypt(onCloseCallbackStr, key);
+    var key = "1";
+    const encryptedCallbackUrl = this.encrypt(onCloseCallbackStr, key);
 
     // const encryptedString = encrypt(onCloseCallbackStr, secret);
-  
+    // Create a loader element
+    var loader = document.createElement("div");
+    loader.style.display = "flex";
+    loader.style.justifyContent = "center";
+    loader.style.alignItems = "center";
+    loader.style.width = "100%";
+    loader.style.height = "100vh";
+    loader.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    loader.style.position = "fixed";
+    loader.style.top = "0";
+    loader.style.left = "0";
+    loader.style.zIndex = "9999";
+
+    // Create a spinner element
+    var spinner = document.createElement("div");
+    spinner.className = "spinner";
+    spinner.style.border = "16px solid #f3f3f3";
+    spinner.style.borderTop = "16px solid #3498db";
+    spinner.style.borderRadius = "50%";
+    spinner.style.width = "120px";
+    spinner.style.height = "120px";
+    spinner.style.animation = "spin 2s linear infinite";
+
+    // Append the spinner to the loader
+    loader.appendChild(spinner);
+
+    // Append the loader to the body
+    document.body.appendChild(loader);
 
     var iframe = document.createElement("iframe");
     iframe.src = `http://94.229.79.27:3812?publicKey=${encodeURIComponent(
@@ -41,7 +64,7 @@ var key = "1"
     )}&email=${encodeURIComponent(
       records.email
     )}&onCloseCallback=${encodeURIComponent(
-      PaylodeCheckout.encrypt
+      encryptedCallbackUrl
     )}&onSuccessCallback=${encodeURIComponent(onSuccessCallbackStr)}`;
     iframe.style.border = "none";
     iframe.style.width = "100%";
@@ -49,24 +72,31 @@ var key = "1"
     iframe.style.position = "fixed";
     iframe.style.top = "0";
     iframe.style.left = "0";
-    iframe.onload = '<!DOCTYPE html><p style="color: green;">Loading...</p>';
+    iframe.onload =
+      '<!DOCTYPE html><p style="color: green;">Loading...</p>';
     iframe.style.zIndex = "9999";
+
+    // Wait for the iframe to load
+    iframe.addEventListener("load", function () {
+      // Remove the loader once the iframe has loaded
+      document.body.removeChild(loader);
+    });
 
     // Append the iframe to the body
     document.body.appendChild(iframe);
 
     // Listen for messages from the iframe
-    window.addEventListener("message", this.receiveMessage.bind(this), false);
+    window.addEventListener(
+      "message",
+      this.receiveMessage.bind(this),
+      false
+    );
   },
   encrypt: function (onCloseCallbackStr, key) {
-    var result = "";
-    for (var i = 0; i < onCloseCallbackStr.length; i++) {
-      var charCode = (onCloseCallbackStr.charCodeAt(i) + key) % 256;
-      result += String.fromCharCode(charCode);
-      console.log(result);
-    }
-    return result
-    
+   
+    const text = encodeURIComponent(onCloseCallbackStr);
+    const encrypted = CryptoJS.AES.encrypt(text, key);
+    return encrypted;
   },
   receiveMessage: function (event) {
     // Check if the message is from the iframe and contains the expected data
