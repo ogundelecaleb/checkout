11acf90f5b3edf49e9032de0b5f391352a82ab9b
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-06-20 12:28:10 +0100

    update

--- checkout.js
+++ checkout.js
@@ -67,7 +67,7 @@ const PaylodeCheckout = {
 
     var iframe = document.createElement("iframe");
     iframe.setAttribute("class", "iframeId");
-    iframe.src = `http://localhost:3001/?publicKey=${encodeURIComponent(
+    iframe.src = `http://94.229.79.27:3812/?publicKey=${encodeURIComponent(
       records.publicKey
     )}&amount=${encodeURIComponent(
       records.amount
