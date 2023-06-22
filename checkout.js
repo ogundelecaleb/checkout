033edbf6ac1702cd146a1bff134b703f16f51655
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-06-22 14:07:07 +0100

    vv

--- checkout.js
+++ checkout.js
@@ -12,7 +12,7 @@ const PaylodeCheckout = {
     console.log(records);
     return this;
   },
-  closewidget: function ({ openIframe }) {
+  closewidget: function ( openIframe ) {
     // const widgetIframe = document.querySelector(".iframeId")
     // widgetIframe.style.display = "none";
     console.log("widget:", openIframe);
